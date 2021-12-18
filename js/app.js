9bb2d496047e2290a1bad5ac52410a9522c0ee2f
pasabaa <sanchez.ballesteros.pablo@gmail.com>
2021-12-17 23:17:15 -0600

    dark mode icons

--- js/app.js
+++ js/app.js
@@ -6,9 +6,12 @@ function changeDarkMode(){
 
   var content = document.getElementById('content');
   var card = document.getElementsByClassName('card');
+  var icon = document.getElementById('switch');
+
   var i;
 
   content.classList.toggle('dark-theme');
+  icon.classList.toggle('bi-toggle2-on')
 
   for (let i = 0; i < card.length; i++) {
     card[i].classList.toggle('bg-dark-variant');
