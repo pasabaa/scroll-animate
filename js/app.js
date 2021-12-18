6a0b066e40f5494d2cd4373470f3d18100ef5292
pasabaa <sanchez.ballesteros.pablo@gmail.com>
2021-12-17 23:00:41 -0600

    dark mode

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 AOS.init({
   delay:200
 });
+
+function changeDarkMode(){
+
+  var content = document.getElementById('content');
+  var card = document.getElementsByClassName('card');
+  var i;
+
+  content.classList.toggle('dark-theme');
+
+  for (let i = 0; i < card.length; i++) {
+    card[i].classList.toggle('bg-dark-variant');
+  }
+
+}
